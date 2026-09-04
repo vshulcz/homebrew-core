2bcb2c9710be0b2f1f6206ad562c2aa860115274
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-04 13:58:58 +0000

    cgal 6.2.1

--- Formula/c/cgal.rb
+++ Formula/c/cgal.rb
@@ -1,8 +1,8 @@
 class Cgal < Formula
   desc "Computational Geometry Algorithms Library"
   homepage "https://www.cgal.org/"
-  url "https://github.com/CGAL/cgal/releases/download/v6.2/CGAL-6.2.tar.xz"
-  sha256 "fbc32816745e871a5cbdeb6245317e9dbf10ae1a957b0ab1edb00b4fde00ba8d"
+  url "https://github.com/CGAL/cgal/releases/download/v6.2.1/CGAL-6.2.1.tar.xz"
+  sha256 "b6be77c60765a8456335de991eeaf6ffec55256984e4a9ecc6a97c37bbfe85bf"
   license "GPL-3.0-or-later"
   compatibility_version 1
 
